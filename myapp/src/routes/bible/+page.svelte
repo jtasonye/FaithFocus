cfa8fef2e84a0746bee49664365768efdcbee4d5
Jae Min Lee <jaemin.lee@gordon.edu>
2023-11-01 23:11:45 -0400

    up to date

--- myapp/src/routes/bible/+page.svelte
+++ myapp/src/routes/bible/+page.svelte
@@ -1,105 +1,103 @@
 <script>
 	/** JSDoc comments used to provide type hints for the variable
-     * `books`
-     * @type {Object.<string, number>}
-     */
-     let books = {
-        // Old Testament
-        "Genesis": 50, 
-        "Exodus": 40, 
-        "Leviticus": 27, 
-        "Numbers": 36,
-        "Deuteronomy": 34,
-        "Joshua": 24,
-        "Judges": 21,
-        "Ruth": 4,
-        "1 Samuel": 31,
-        "2 Samuel": 24,
-        "1 Kings" : 22,
-        "2 Kings" : 25,
-        "1 Chronicles" : 29,
-        "2 Chronicles" : 36,
-        "Ezra" : 10,
-        "Nehemiah" : 13,
-        "Esther" : 10,
-        "Job" : 42,
-        "Psalms": 150,
-        "Proverbs": 31,
-        "Ecclesiastes" : 12,
-        "Song of Songs": 8,
-        "Isaiah": 66,
-        "Jeremiah" : 52,
-        "Lamentations" : 5,
-        "Ezekiel" : 48,
-        "Daniel" : 12,
-        "Hosea" : 14,
-        "Joel" : 3,
-        "Amos" : 9,
-        "Obadiah" : 1,
-        "Jonah" : 4,
-        "Micah" : 7,
-        "Nahum" : 3,
-        "Habakkuk" : 3,
-        "Zephaniah" : 3,
-        "Haggai" : 2,
-        "Zechariah" : 14,
-        "Malachi" : 4,
-    
-        // New Testament
-        "Matthew" : 28,
-        "Mark" : 16,
-        "Luke" : 24,
-        "John" : 21,
-        "Acts" : 28,
-        "Romans" : 16,
-        "1 Corinthians" : 16,
-        "2 Corinthians" : 16,
-        "Galatians" : 6,
-        "Ephesians" : 6,
-        "Philippians" : 4,
-        "Colossians" : 4,
-        "1 Thessalonians" : 5,
-        "2 Thessalonians" : 3,
-        "1 Timothy" : 6,
-        "2 Timothy" : 4,
-        "Titus" : 3,
-        "Philemon" : 1,
-        "Hebrews" : 13,
-        "James" : 5,
-        "1 Peter" : 5,
-        "2 Peter" : 3,
-        "1 John" : 5,
-        "2 John" : 1,
-        "3 John" : 1,
-        "Jude" : 1,
-        "Revalation" : 22
-    };
-    
-    /** JSDoc comments used to provide type hints for the variable
-     * `selectedBook`
-     * @type {string}
-     */
-    let selectedBook = '';
-    
-    /** JSDoc comments used to provide type hints for the variable
-     * `chapters`.
-     * @type {string[]}
-     */
-    let chapters = [];
+	 * `books`
+	 * @type {Object.<string, number>}
+	 */
+	let books = {
+		// Old Testament
+		Genesis: 50,
+		Exodus: 40,
+		Leviticus: 27,
+		Numbers: 36,
+		Deuteronomy: 34,
+		Joshua: 24,
+		Judges: 21,
+		Ruth: 4,
+		'1 Samuel': 31,
+		'2 Samuel': 24,
+		'1 Kings': 22,
+		'2 Kings': 25,
+		'1 Chronicles': 29,
+		'2 Chronicles': 36,
+		Ezra: 10,
+		Nehemiah: 13,
+		Esther: 10,
+		Job: 42,
+		Psalms: 150,
+		Proverbs: 31,
+		Ecclesiastes: 12,
+		'Song of Songs': 8,
+		Isaiah: 66,
+		Jeremiah: 52,
+		Lamentations: 5,
+		Ezekiel: 48,
+		Daniel: 12,
+		Hosea: 14,
+		Joel: 3,
+		Amos: 9,
+		Obadiah: 1,
+		Jonah: 4,
+		Micah: 7,
+		Nahum: 3,
+		Habakkuk: 3,
+		Zephaniah: 3,
+		Haggai: 2,
+		Zechariah: 14,
+		Malachi: 4,
 
+		// New Testament
+		Matthew: 28,
+		Mark: 16,
+		Luke: 24,
+		John: 21,
+		Acts: 28,
+		Romans: 16,
+		'1 Corinthians': 16,
+		'2 Corinthians': 16,
+		Galatians: 6,
+		Ephesians: 6,
+		Philippians: 4,
+		Colossians: 4,
+		'1 Thessalonians': 5,
+		'2 Thessalonians': 3,
+		'1 Timothy': 6,
+		'2 Timothy': 4,
+		Titus: 3,
+		Philemon: 1,
+		Hebrews: 13,
+		James: 5,
+		'1 Peter': 5,
+		'2 Peter': 3,
+		'1 John': 5,
+		'2 John': 1,
+		'3 John': 1,
+		Jude: 1,
+		Revalation: 22
+	};
 
-    // Loads the chapter value according to the book
-    function populateChapters() {
-        chapters = [];
-        const selectedBookChapters = books[selectedBook];
-        if (selectedBookChapters) {
-            for (let i = 1; i <= selectedBookChapters; i++) {
-                chapters.push(`Chapter ${i}`);
-            }
-        }
-    }
+	/** JSDoc comments used to provide type hints for the variable
+	 * `selectedBook`
+	 * @type {string}
+	 */
+	let selectedBook = '';
+
+	/** JSDoc comments used to provide type hints for the variable
+	 * `chapters`.
+	 * @type {string[]}
+	 */
+	let chapters = [];
+
+	// Loads the chapter value according to the book
+	function populateChapters() {
+		chapters = [];
+		const selectedBookChapters = books[selectedBook];
+		if (selectedBookChapters) {
+			for (let i = 1; i <= selectedBookChapters; i++) {
+				chapters.push(`Chapter ${i}`);
+			}
+		}
+	}
 </script>
-
 
 <div class="bible-container">
 	<div id="portrait-bible">
@@ -125,37 +123,33 @@
 	</div>
 
 	<div id="notes-panel">
-		<p>
-			Need to figure out how anything works!
-		</p>
+		<p>Need to figure out how anything works!</p>
 	</div>
-
 </div>
 
 <style>
 	.bible-container {
-            display: flex;
-            height: 100vh; /* Set the height of the container to the full viewport height */
-        }
+		display: flex;
+		height: 100vh; /* Set the height of the container to the full viewport height */
+	}
 
-        #portrait-bible, #notes-panel {
-            width: 50%; /* Divide the container into two equal parts */
-            height: 100%; /* Fill the entire height of the container */
-			background-color: #f0f0f0;
-            border: 1px solid #ccc; /* Adding a border for visual separation */
-            box-sizing: border-box; /* Include borders in width calculation */
-            overflow: auto; /* Enable scrolling if content overflows */
-			border-radius: 15px; 
-        }
+	#portrait-bible,
+	#notes-panel {
+		width: 50%; /* Divide the container into two equal parts */
+		height: 100%; /* Fill the entire height of the container */
+		background-color: #f0f0f0;
+		border: 1px solid #ccc; /* Adding a border for visual separation */
+		box-sizing: border-box; /* Include borders in width calculation */
+		overflow: auto; /* Enable scrolling if content overflows */
+		border-radius: 15px;
+	}
 
-		.all-books, .chapter-option{
-			margin-left: 10px;
-			padding-top: 10px;
-			float:left; 
-			display:inline; 
-			width: 25%;
-		}
-
-
+	.all-books,
+	.chapter-option {
+		margin-left: 10px;
+		padding-top: 10px;
+		float: left;
+		display: inline;
+		width: 25%;
+	}
 </style>
-  
